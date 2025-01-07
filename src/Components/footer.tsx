b6df2f65d5c6b00779af4939b93d8e82dcb7e5a7
BridgetKas <bridgetnamugga@gmail.com>
2025-01-07 11:49:38 +0300

    Added footer to landing page

--- src/Components/footer.tsx
+++ src/Components/footer.tsx
@@ -5,11 +5,11 @@ import { FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <footer className='flex items-center justify-between bg-gray-800 text-white gap-1.5'>
+    <footer className='flex items-center justify-between bg-gray-800 text-white p-3'>
         <div>
-            <p className="flex flex-row items-center">Copyright <FaRegCopyright />2025 Namugga Bridget. All rights reserved.</p>
+            <p className="flex  flex-wrap sm:flex-row items-center gap-1.5">Copyright <FaRegCopyright />2025 Namugga Bridget. All rights reserved.</p>
         </div>
-        <div>
+        <div className="flex flex-row text-[16px] items-center gap-1.5 sm:text-xl">
             <FaLinkedin />
             <FaGithub />
             <FaXTwitter />
