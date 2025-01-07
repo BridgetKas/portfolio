1fff112e5f930a4e39cbaf7411c32e2a6945e5da
BridgetKas <bridgetnamugga@gmail.com>
2025-01-07 11:34:01 +0300

    Added footer

--- src/Components/footer.tsx
+++ src/Components/footer.tsx
@@ -1,11 +1,19 @@
 import { FaRegCopyright } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 function Footer() {
   return (
     <footer>
         <div>
             Copyright <FaRegCopyright />2025 Namugga Bridget. All rights reserved.
         </div>
-        <div></div>
+        <div>
+            <FaLinkedin />
+            <FaGithub />
+            <FaXTwitter />
+        </div>
     </footer>
   )
 }
