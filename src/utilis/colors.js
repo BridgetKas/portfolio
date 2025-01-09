332bac56b9b4de2ae45d0f843f0a99c3564c2985
BridgetKas <bridgetnamugga@gmail.com>
2025-01-09 15:33:27 +0300

    Added colors object

---
+++ src/utilis/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    primary: "#3b5d50",
+    secondary: "#eff2f1",
+    yellow: "#f9bf29",
+    black: "#2f2f2f",
+    white: "#fff",
+    whiteGradient: "rgba(255, 255, 255, 0.5)",
+    darkGray: "#6a6a6a",
+    paleBlue:"#adb5bd",
+
+}
