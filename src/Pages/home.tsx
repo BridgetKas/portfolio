329e9239eef159b14b5fc1b1d5d33c015c3b0530
BridgetKas <bridgetnamugga@gmail.com>
2025-01-11 10:19:04 +0300

    Added mobile nav

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -2,8 +2,8 @@ import RotatingWheel from "../Components/rotatingWheel";
 
 function HomePage() {
   return (
-    <div className='flex flex-col items-center gap-[60px] w-[98%] mx-auto mt-[40px]  sm:flex-row sm:justify-between'>
-      <div className=' w-full  sm:w-[40%]'>
+    <div className='flex flex-col items-center gap-[60px]  mt-[40px]  w-[90%] mx-auto sm:flex-row sm:justify-between'>
+      <div className=' w-full sm:w-[40%]'>
         <p>Hello </p>
         <p>I'm Bridget Namugga a front end developed based in Uganda.
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
