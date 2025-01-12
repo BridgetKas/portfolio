09351afc3af0339fa2b19ed61268f854d399127f
BridgetKas <bridgetnamugga@gmail.com>
2025-01-12 16:52:06 +0300

    Modified Landing page

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -6,7 +6,8 @@ function HomePage() {
       <div className='w-full sm:w-[40%] bg-purple-400'>
         <p className=''>Hello I'm  </p>
         <p className='text-[45px] font-bold'> <span className='text-yellow '>Bridget Namugga. </span></p>
-        <p>a dedicated front-end developer with a passion for crafting  responsive, and user-friendly web applications ensuring a seamless user experience.  </p>
+        <p>a dedicated front-end developer with a passion for crafting  responsive, and user-friendly web applications ensuring a seamless user experience.
+          Technologically, I possess expertise in mordern frameworks and technologies like Javascript,React, TypeScript, and Tailwind CSS </p>
       </div>
       <div className='w-full sm:w-[60%] bg-red-500'>
         <RotatingWheel />
