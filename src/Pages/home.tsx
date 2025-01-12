79cf0d2c2edcb2fe438788e12866ace3e4d03253
BridgetKas <bridgetnamugga@gmail.com>
2025-01-12 18:01:59 +0300

    Modified Landing page

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <div className='flex flex-col items-center mt-[20px] gap-[20px] w-[90%] mx-auto sm:flex-row sm:mt-[40px] sm:justify-between'>
       <div className='w-full sm:w-[40%] bg-purple-400'>
-        <p className=''>Hello I'm  </p>
+        <p className='text-[20px]'>HELLO I'M  </p>
         <p className='text-[40px] font-bold text-yellow'>Bridget Namugga.</p>
         <p>a dedicated front-end developer with a passion for crafting  responsive, and user-friendly web applications ensuring a seamless user experience.</p>
         <p>Technologically, I possess expertise in mordern frameworks and technologies like Javascript,React, TypeScript, and Tailwind CSS.</p>
