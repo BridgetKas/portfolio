e5433c861871d96a889d1546ed6292fa108d4111
BridgetKas <bridgetnamugga@gmail.com>
2025-01-12 16:47:42 +0300

    Modified Landing page

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -3,12 +3,12 @@ import RotatingWheel from "../Components/rotatingWheel";
 function HomePage() {
   return (
     <div className='flex flex-col items-center mt-[20px] gap-[20px] w-[90%] mx-auto sm:flex-row sm:mt-[40px] sm:justify-between'>
-      <div className='w-full sm:w-[40%]'>
-        <p>Hello there </p>
-        <p>I'm Bridget Namugga a front end developer.
-        </p>
+      <div className='w-full sm:w-[40%] bg-purple-400'>
+        <p className=''>Hello I'm  </p>
+        <p className='text-[45px] font-bold'> <span className='text-yellow '>Bridget Namugga. </span></p>
+        <p>a dedicated front-end developer with a passion for crafting  responsive, and user-friendly web applications ensuring a seamless user experience.  </p>
       </div>
-      <div className='w-full sm:w-[60%]'>
+      <div className='w-full sm:w-[60%] bg-red-500'>
         <RotatingWheel />
       </div>
     
