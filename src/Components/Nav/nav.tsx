294835cec751b46caa827d3d622ff8a942963d11
BridgetKas <bridgetnamugga@gmail.com>
2025-01-13 17:14:46 +0300

    Modified Landing page

--- src/Components/Nav/nav.tsx
+++ src/Components/Nav/nav.tsx
@@ -4,7 +4,7 @@ import MobileNav from "./mobileNav";
 
 function Nav() {
   return (
-    <div className="flex  items-center justify-between w-[90%] mx-auto mt-3 bg-secondary py-4 px-2.5 text-primary rounded-full">
+    <div className="flex  items-center justify-between w-[95%] mx-auto mt-3 bg-secondary py-4 px-2.5 text-primary rounded-full">
         <p>LOGO</p>
         <DesktopNav/>
         <MobileNav/>
