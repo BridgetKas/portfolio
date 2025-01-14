8af93c3484e1dde8197c67adee3c71e2a17cf0ad
BridgetKas <bridgetnamugga@gmail.com>
2025-01-14 16:21:40 +0300

    Added contact page

--- src/Pages/contact.tsx
+++ src/Pages/contact.tsx
@@ -1,7 +1,41 @@
-
+import {useState,useReducer} from 'react'
 function ContactPage() {
+  const [name,setName] =useState('')
   return (
-    <div>ContactPage</div>
+    <div className='flex items-start justify-between mt-10 w-[95%] mx-auto'>
+      <div>
+        <p>Let's discuss your next project</p>
+      </div>
+      <form className='flex flex-col bg-red-500 w-[60%] p-4'>
+        <div className='flex '>
+          <div>
+            <label> First Name</label>
+            <input type='text' placeholder="Enter your first name" value={name} className='input' required/>
+          </div>
+          <div>
+            <label>Last Name</label>
+            <input type='text' placeholder="Enter your last name" value={name} className='input'required/>  
+          </div>
+          
+        </div>
+        <div>
+          <label> Email </label>
+          <input type='email' placeholder="Enter your email" value={name} className='input'required/>
+        </div>
+        <div>
+          <label>Phone Number</label>
+          <input type='tel' placeholder="Enter your phone number" value={name} className='input' required/>
+        </div>
+        <label> I'm intersted in <br/></label>
+        <select className='input'>
+            <option>Landing Page</option>
+            <option>Blog Website</option>
+            <option>Portfolio</option>
+            <option>Website</option>
+          </select>
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -8,7 +8,7 @@ function HomePage() {
         <p className='mb-3 '>Hello, welcome I'm a dedicated front-end developer with a passion for crafting  responsive, and user-friendly web applications ensuring a seamless user experience.</p>
         <p className='mb-3'>Technologically, I possess expertise in mordern frameworks and technologies like Javascript,React, TypeScript, and Tailwind CSS.As a driven individual, I continously strive to explore new domains within technology.</p>
         <p>Explore my work, and let's work together!</p>
-        <button className='text-center p-2 bg-secondary py-1 px-2.5 text-primary mt-5 rounded-full'>Hire me</button>
+        <button className='text-center p-2 bg-yellow py-1 px-2.5 text-black mt-5 rounded-full'>Hire me</button>
       </div>
       <div className='w-full sm:w-[58%] self-start'>
         <RotatingWheel />
