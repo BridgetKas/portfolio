15d402e194348b9a27ed1d0b8303c2885aebe597
BridgetKas <bridgetnamugga@gmail.com>
2025-01-15 14:24:41 +0300

    Added contact page

--- src/Pages/contact.tsx
+++ src/Pages/contact.tsx
@@ -10,7 +10,7 @@ function ContactPage() {
   return (
     <div className='flex flex-col items-start justify-between mt-10 w-[95%] mx-auto'>
       <div className='bg-pink-600'>
-        <p className=' text-[40px] leading-snug font-medium'>Let's Discuss <span className='text-yellow font-normal'>Your Next Project </span></p>
+        <h1 className=' text-[40px] leading-snug font-medium'>Let's Connect and Build your Next Project </h1>
         <div className="flex flex-row text-[16px]  gap-2.5 sm:text-xl bg-yellow items-center justify-center p-4">
           <FaLinkedin />
           <FaGithub />
@@ -20,30 +20,30 @@ function ContactPage() {
       <form className='flex flex-col bg-red-500 w-full p-4'>
         <div className='flex flex-col items-center'>
           <div className='w-full'>
-            <label> First Name</label>
+            <label className='label'> First Name</label>
             <input type='text' placeholder="Enter your first name" value={name} className='input' required/>
           </div>
-          <div>
-            <label>Last Name</label>
+          <div className='bg-purple-950'>
+            <label className='label'>Last Name</label>
             <input type='text' placeholder="Enter your last name" value={name} className='input'required/>  
           </div>
         </div>
         <div>
-          <label> Email </label>
+          <label className='label'> Email </label>
           <input type='email' placeholder="Enter your email" value={name} className='input'required/>
         </div>
         <div>
-          <label>Phone Number</label>
+          <label className='label'>Phone Number</label>
           <input type='tel' placeholder="Enter your phone number" value={name} className='input' required/>
         </div>
-        <label> I'm intersted in <br/></label>
+        <label className='label'> I'm intersted in <br/></label>
         <select className='input'>
             <option>Landing Page</option>
             <option>Blog Website</option>
             <option>Portfolio</option>
             <option>Website</option>
           </select>
-          <textarea className='input h-[200px] rounded-xl'></textarea>
+          <textarea className='input h-[200px] rounded-xl' placeholder='Enter your message'></textarea>
         <button>Submit</button>
       </form>
     </div>
