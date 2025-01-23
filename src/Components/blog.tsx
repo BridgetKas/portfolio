7effd784f23a62c9008e691a1ef9fbe0b2fe11af
BridgetKas <bridgetnamugga@gmail.com>
2025-01-23 16:39:23 +0300

    modified the blog card

--- src/Components/blog.tsx
+++ src/Components/blog.tsx
@@ -3,20 +3,21 @@ type propsType = {
     title:string,
     description:string,
     image:string| undefined,
+    readmore:string
 }
 export default function MediaCard(props:propsType) {
-    const {title,description,image} = props
+    const {title,description,image,readmore} = props
   return (
-    <div className='p-4 border border-red-700 shadow-xl'>
-        <div>
-            <img src={image}/>
+    <div className='flex flex-col gap-2 w-[90%] h-[390px] mx-auto mt-3 rounded-t-xl shadow-2xl  sm:w-[45%]  hover:scale-105 transform  transition-all duration-500 ease-in-out'>
+        <div className='h-[60%] w-full'>
+            <img src={image} className='h-full w-full rounded-t-lg'/>
         </div>
         <div>
             <h5>{title}</h5>
             <p>{description}</p>
         </div>
         <div>
-            <a href='#' target='_blank'>Read more</a>
+            <a href={readmore} target='_blank'>Read more</a>
         </div>
     </div>
   );
--- src/Pages/blog.tsx
+++ src/Pages/blog.tsx
@@ -2,11 +2,11 @@ import MediaCard from "../Components/blog"
 
 function BlogPage() {
   return (
-    <div className='flex flex-col mt-2 w-[90%] mx-auto md:flex-row gap-2 sm:flex-row sm:flex-wrap'>
-      <MediaCard title="Try me" description="me" image="./demisfy.png"/>
-      <MediaCard title="Try me" description="me" image="gall"/>
-      <MediaCard title="Hoisting" description="Coming Soon..." image="./hoisting.png"/>
-      <MediaCard title="Promises" description="Coming Soon..." image="./promises.jpg"/>
+    <div className='flex flex-col w-[95%] mx-auto sm:flex-row sm:gap-2.5 sm:flex-wrap mb-[60px] lg:w-[85%]'>
+      <MediaCard title="Try me" description="me" image="./js2.jpg" readmore="#"/>
+      <MediaCard title="Try me" description="me" image="./begineerJS1.jpg" readmore="#"/>
+      <MediaCard title="Promises" description="Coming Soon..." image="./promises.jpg" readmore="#"/>
+      <MediaCard title="Hoisting" description="Coming Soon..." image="./hoisting.png" readmore="#"/>
 
     </div>
 
