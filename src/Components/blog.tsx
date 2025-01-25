0d3f0c1307db0a96daa00f8ac7a3fa0043a7e61d
BridgetKas <bridgetnamugga@gmail.com>
2025-01-25 04:15:41 +0300

    changed the blog card styling

--- src/Components/blog.tsx
+++ src/Components/blog.tsx
@@ -1,23 +1,30 @@
+import { BiBookReader } from "react-icons/bi";
 
 type propsType = {
     title:string,
     description:string,
     image:string| undefined,
     readmore:string
+    date:string,
+    time:string
 }
 export default function MediaCard(props:propsType) {
-    const {title,description,image,readmore} = props
+    const {title,description,image,readmore,date,time} = props
   return (
-    <div className='flex flex-col gap-2 w-[90%] h-[390px] mx-auto mt-3 rounded-t-xl shadow-2xl  sm:w-[45%]  hover:scale-105 transform  transition-all duration-500 ease-in-out'>
-        <div className='h-[60%] w-full'>
-            <img src={image} className='h-full w-full rounded-t-lg'/>
-        </div>
-        <div>
-            <h5>{title}</h5>
+    <div className='flex flex-col gap-2 p-2 w-[98%] h-[390px] bg-red-500 mx-auto mt-3 shadow-2xl  hover:scale-105 transform  transition-all duration-500 ease-in-out'>
+        <div className='flex flex-col gap-2 w-full bg-yellow'>
+            <h1 className='text-secondary font-bold '>{title}</h1> 
+            <div className='flex  items-center justify-between bg-purple-500 '>
+                <p>{date}</p>
+                <p className='flex items-center gap-2'><BiBookReader /> {time}</p>
+            </div>
             <p>{description}</p>
-        </div>
-        <div>
+            <div>
             <a href={readmore} target='_blank'>Read more</a>
+            </div>
+        </div>
+        <div className='w-full'>
+            <img src={image} className=' rounded-t-lg h-[390px]'/>
         </div>
     </div>
   );
--- src/Pages/blog.tsx
+++ src/Pages/blog.tsx
@@ -2,11 +2,27 @@ import MediaCard from "../Components/blog"
 
 function BlogPage() {
   return (
-    <div className='flex flex-col w-[95%] mx-auto sm:flex-row sm:gap-2.5 sm:flex-wrap mb-[60px] lg:w-[85%]'>
-      <MediaCard title="Try me" description="me" image="./js2.jpg" readmore="#"/>
-      <MediaCard title="Try me" description="me" image="./begineerJS1.jpg" readmore="#"/>
-      <MediaCard title="Promises" description="Coming Soon..." image="./promises.jpg" readmore="#"/>
-      <MediaCard title="Hoisting" description="Coming Soon..." image="./hoisting.png" readmore="#"/>
+    <div >
+      <div className='flex flex-col items-center justify-center '>
+        <div>
+         <img 
+          src="https://api.dicebear.com/9.x/open-peeps/svg?seed=Riley&accessories=sunglasses2,sunglasses,glasses5,glasses4,glasses3,glasses2,glasses,eyepatch&face=awe,concerned,concernedFear,contempt,cute,cyclops,driven,eatingHappy,explaining,eyesClosed,fear,hectic,lovingGrin1,lovingGrin2,monster,old,rage,serious,smile,smileBig,smileLOL,smileTeethGap,solemn,suspicious,tired,veryAngry,cheeky,angryWithFang,blank&facialHair=goatee2,moustache1,moustache2,moustache3,moustache4,moustache5,moustache6,moustache7,moustache8,moustache9&backgroundColor=transparent" 
+          alt="Bridget's avatar"
+          className="h-[150px] w-[150px] bg-yellow rounded-full"
+          />
+        </div>
+        <p className="text-center">Self taught developer figuring out this coding thing one line at a time. </p>
+      </div>
+      <div className='flex  w-[95%] mx-auto sm:flex-row sm:gap-2.5 sm:flex-wrap mb-[60px] lg:w-[85%]'>
+        <MediaCard 
+        title="Javascript Closures" 
+        description="A scope is a region in a function where you can access a variable. Look at the following piece of code. 
+        Variables are accessible in the scope where they are defined and in any nested scopes." 
+        image="./js2.jpg" readmore="#"date="Dec 9 2024" time="3 min"/>
+        {/* <MediaCard title="Try me" description="me" image="./begineerJS1.jpg" readmore="#"/> */}
+        {/* <MediaCard title="Promises" description="Coming Soon..." image="./promises.jpg" readmore="#"/> */}
+        {/* <MediaCard title="Hoisting" description="Coming Soon..." image="./hoisting.png" readmore="#"/> */}
+      </div>
     </div>
 
   )
