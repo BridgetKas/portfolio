6d2b8955b36da88d45f99ad31ff54e9399db1649
BridgetKas <bridgetnamugga@gmail.com>
2025-02-04 11:17:02 +0300

    Added dark and light theme to mobile nav

--- src/Components/Nav/mobileNav.tsx
+++ src/Components/Nav/mobileNav.tsx
@@ -1,10 +1,9 @@
-import {  useState } from 'react'
+import {  useState, useEffect} from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { NavLink } from 'react-router-dom'
 import styles from './nav.module.css'
 import { FaRegMoon } from "react-icons/fa";
-// import ThemeProvider from '../ThemeContext';
-
+import { MdOutlineWbSunny } from "react-icons/md";
 
 
 const navbar = [
@@ -35,7 +34,16 @@ const body:bodyType= document.querySelector('body')
 
 function MobileNav() {
     const [isOpen,setisOpen] = useState(false)
-    // const {darkTheme , toggleTheme} = useContext(ThemeProvider)
+    const [dark,setDark] = useState(localStorage.getItem('Darktheme') === 'isTrue' ? true : false)
+    
+    useEffect(() => {
+    const userTheme = localStorage.getItem('Darktheme')
+    if(userTheme === 'isTrue'){
+        document.documentElement.classList.add('dark')
+    }else {
+        document.documentElement.classList.remove('dark')
+    }
+    },[dark])                   
 
     const menuClicked = () => {
       setisOpen(!isOpen)
@@ -44,28 +52,32 @@ function MobileNav() {
       }
     }
 
+    const handleToggle = () => {
+      setDark(!dark)
+      if(dark) {
+        document.documentElement.classList.remove('dark')
+        localStorage.setItem('Darktheme', 'isFalse')
+      }else{
+        document.documentElement.classList.add('dark')
+        localStorage.setItem('Darktheme', 'isTrue')
+      }
+    }
+
     const linkClicked=() =>{
       setisOpen(!isOpen)
     }
-
-    const handleClick = () => {
-      
-
-    }
   
     return (
-      <div className="flex items-center justify-between w-[95%] mx-auto mt-3 bg-secondary py-4 px-2.5 text-primary rounded-full sm:hidden">
-        {/* <p>LOGO</p> */}
-        <div className='p-2'>
-         <FaRegMoon size={28} onClick={handleClick}/>
-        </div>
+      <div className="flex items-center justify-between w-[95%] mx-auto mt-3  bg-secondary py-4 px-2.5 text-primary rounded-full sm:hidden">
+        <button className='p-2' onClick={handleToggle}>
+          {dark? <FaRegMoon size={28}/> : <MdOutlineWbSunny size={30}/>}
+        </button>
         <div className='flex flex-col  '>
           <GiHamburgerMenu size={30} color='#2f2f2f' onClick={menuClicked} /> 
             <div className={`${styles.mobileDropdown} ${isOpen ? styles.open: ""}`}>
               {navbar.map(item => 
                 <NavLink to={item.path} key={item.path} className='my-0 mx-[0.6rem] p-1.5 text-[18px] text-right text-primary w-full bg-secondary' onClick={linkClicked}>{item.label}</NavLink>
               )}
-              {/* <div className='bg-primary/0 w-full h-[40vh] '></div> */}
             </div>
         </div>
       </div>
