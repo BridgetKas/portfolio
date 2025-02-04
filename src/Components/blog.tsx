e8cc56771cd0713a87798ecae677d0a283412049
BridgetKas <bridgetnamugga@gmail.com>
2025-02-04 16:33:40 +0300

    added dark theme in contactComponet

--- src/Components/blog.tsx
+++ src/Components/blog.tsx
@@ -11,16 +11,16 @@ type propsType = {
 export default function MediaCard(props:propsType) {
     const {title,description,image,readmore,date,time} = props
   return (
-    <div className=' mb-[10px] flex flex-col items-center gap-2 p-2 w-[90%] h-[440px] mx-auto mt-4 shadow-2xl sm:flex-row sm:h-[320px] md:h-[280px] lg:h-[220px] lg:w-[88%] hover:scale-105 transform  transition-all duration-500 ease-in-out'>
+    <div className=' mb-[10px] flex flex-col items-center gap-2 p-2 w-[90%] h-[440px] mx-auto mt-4 shadow-2xl sm:flex-row sm:h-[320px] md:h-[280px] lg:h-[220px] lg:w-[88%] hover:scale-105 transform  transition-all duration-500 ease-in-out dark:bg-darkFootNav'>
         <div className='flex flex-col gap-2 w-full'>
-            <h1 className='text-[#0fa81c] font-bold text-[30px]'>{title}</h1> 
+            <h1 className='text-[#0fa81c] font-bold text-[30px]  dark:text-yellow'>{title}</h1> 
             <div className='flex  items-center justify-between '>
                 <p>{date}</p>
                 <p className='flex items-center gap-2'><BiBookReader /> {time}</p>
             </div>
             <p className=''>{description}</p>
             <div>
-                <a href={readmore} target='_blank' className=' text-blue-950 hover:underline focus:text-purple-800'>Read more</a>
+                <a href={readmore} target='_blank' className=' text-blue-950 hover:underline focus:text-purple-800 dark:text-blue-600'>Read more</a>
             </div>
         </div>
         <div className='w-full flex items-center justify-center'>
--- src/Pages/contact.tsx
+++ src/Pages/contact.tsx
@@ -57,14 +57,14 @@ function ContactPage() {
   return (
     <div className='flex flex-col mt-2 w-[95%] mx-auto md:flex-row lg:w-[75%] mb-[60px]'>
       <div className='md:w-[50%]' >
-        <h1 className=' text-[40px] leading-snug p-4 text-center md:text-left'>Let's Connect and Build your <span className='text-yellow font-medium'>Next Project</span></h1>
+        <h1 className=' text-[40px] leading-snug p-4 text-center md:text-left dark:text-whiteGradient'>Let's Connect and Build your <span className='text-yellow font-medium'>Next Project</span></h1>
         <div className="flex flex-row text-[30px] gap-2.5 items-center justify-center p-4 my-10">
           <a href='#' target='_blank' className='hover:scale-110 transform'><FaLinkedin /></a>
           <a href='#' target='_blank' className='hover:scale-110 transform'><FaGithub /></a>
           <a href='#' target='_blank' className='hover:scale-110 transform'><FaXTwitter /></a> 
         </div>
       </div>
-      <form className='flex flex-col p-4 md:w-[50%]  shadow-2xl' onSubmit={handleSubmit}>
+      <form className='flex flex-col p-4 md:w-[50%]  shadow-2xl dark:bg-darkFootNav dark:rounded-xl' onSubmit={handleSubmit}>
         <div >
           <label> Your name<span className='text-red-600'>*</span></label>
           <input  name='name'type='text' placeholder="Enter your first and last name" value={input.name} className='input' onChange={(e) =>handleChange(e)} required/>
