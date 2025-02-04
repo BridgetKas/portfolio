525c35c1a2e14829747849724c6abf41df9cd4ea
BridgetKas <bridgetnamugga@gmail.com>
2025-02-04 16:18:42 +0300

    added dark theme in projectComponet

--- src/Components/projectComponent.tsx
+++ src/Components/projectComponent.tsx
@@ -20,7 +20,7 @@ function ProjectComponent(props:propType) {
     const {image,languagesArray,sourcecode,liveapp,description} = props
 
   return (
-    <div className='flex flex-col gap-2 w-[90%] h-[390px] mx-auto mt-3 rounded-t-xl shadow-2xl  sm:w-[45%]  hover:scale-105 transform  transition-all duration-500 ease-in-out '>
+    <div className='flex flex-col gap-2 w-[90%] h-[390px] mx-auto mt-3 rounded-t-xl shadow-2xl  sm:w-[45%]  hover:scale-105 transform  transition-all duration-500 ease-in-out dark:bg-darkFootNav '>
         <div className='bg-gray-300 h-[60%] w-full rounded-t-lg'>
             <img src={image} className='rounded-t-lg h-full w-full'/>
         </div>
@@ -33,11 +33,10 @@ function ProjectComponent(props:propType) {
                     }} key={item.id}className='text-black border-2 border-transparent outline-none bg-transparent rounded-full text-[16px] font-bold px-2 py-1'>{item.language}</button>)
                 }
             </div>
-            {/* <p>{}</p> */}
-            <p className='leading-[1.4] pt-2'>{description}</p>
+            <p className='leading-[1.4] pt-2 dark:text-whiteGradient'>{description}</p>
             <div className='flex justify-between pb-1'>
-                <a href={sourcecode} target='_blank' className='text-blue-800 text-[16px] hover:underline active:text-violet-700 '>Source code</a>
-                <a href={liveapp}  className='text-blue-800 text-[16px]  active:text-violet-700'>Live App</a>
+                <a href={sourcecode} target='_blank' className='text-blue-800 text-[16px] hover:underline active:text-violet-700 dark:text-blue-600'>Source code</a>
+                <a href={liveapp}  className='text-blue-800 text-[16px]  active:text-violet-700 dark:text-blue-600'>Live App</a>
             </div>
         </div>
         
