05ad87032674fd51db0508916985a1277756fc0c
BridgetKas <bridgetnamugga@gmail.com>
2025-02-06 15:37:43 +0300

    blog card height change

--- src/Components/blog.tsx
+++ src/Components/blog.tsx
@@ -11,7 +11,7 @@ type propsType = {
 export default function MediaCard(props:propsType) {
     const {title,description,image,readmore,date,time} = props
   return (
-    <div className=' mb-[10px] flex flex-col items-center gap-2 p-2 w-[90%] h-[440px] mx-auto mt-4 shadow-2xl sm:flex-row sm:h-[320px] md:h-[280px] lg:h-[220px] lg:w-[88%] hover:scale-105 transform  transition-all duration-500 ease-in-out dark:bg-darkFootNav'>
+    <div className=' mb-[10px] flex flex-col items-center gap-2 p-2 w-[90%] h-[440px] mx-auto mt-4 shadow-2xl sm:flex-row sm:h-[340px] md:h-[300px] lg:h-[250px] lg:w-[88%] hover:scale-105 transform  transition-all duration-500 ease-in-out dark:bg-darkFootNav'>
         <div className='flex flex-col gap-2 w-full'>
             <h1 className='text-[#0fa81c] font-bold text-[30px]  dark:text-yellow'>{title}</h1> 
             <div className='flex  items-center justify-between '>
@@ -19,8 +19,8 @@ export default function MediaCard(props:propsType) {
                 <p className='flex items-center gap-2'><BiBookReader /> {time}</p>
             </div>
             <p className=''>{description}</p>
-            <div>
-                <a href={readmore} target='_blank' className=' text-blue-950 hover:underline dark:text-blue-600 focusLink p-1.5'>Read more</a>
+            <div className='mt-1.5'>
+                <a href={readmore} target='_blank' className=' text-blue-950 underline hover:underline dark:text-blue-600 focusLink p-1.5'>Read more</a>
             </div>
         </div>
         <div className='w-full flex items-center justify-center'>
