10d40968fa3e374043d7976ea25c27a0e6a9679a
BridgetKas <bridgetnamugga@gmail.com>
2025-02-06 11:43:29 +0300

    changed label textColor

--- src/Pages/contact.tsx
+++ src/Pages/contact.tsx
@@ -59,27 +59,27 @@ function ContactPage() {
       <div className='md:w-[50%]' >
         <h1 className=' text-[40px] leading-snug p-4 text-center md:text-left dark:text-whiteGradient'>Let's Connect and Build your <span className='text-yellow font-medium'>Next Project</span></h1>
         <div className="flex flex-row text-[30px] gap-2.5 items-center justify-center p-4 my-10">
-          <a href='#' target='_blank' className='hover:scale-110 transform'><FaLinkedin /></a>
-          <a href='#' target='_blank' className='hover:scale-110 transform'><FaGithub /></a>
-          <a href='#' target='_blank' className='hover:scale-110 transform'><FaXTwitter /></a> 
+          <a href='#' target='_blank' className='hover:scale-110 transform  dark:text-whiteGradient focusIcon p-1'><FaLinkedin /></a>
+          <a href='#' target='_blank' className='hover:scale-110 transform  dark:text-whiteGradient focusIcon p-1'><FaGithub /></a>
+          <a href='#' target='_blank' className='hover:scale-110 transform  dark:text-whiteGradient focusIcon p-1'><FaXTwitter /></a> 
         </div>
       </div>
       <form className='flex flex-col p-4 md:w-[50%]  shadow-2xl dark:bg-darkFootNav dark:rounded-xl' onSubmit={handleSubmit}>
         <div >
-          <label> Your name<span className='text-red-600'>*</span></label>
+          <label className='dark:text-gray-300'> Your name<span className='text-red-600'>*</span></label>
           <input  name='name'type='text' placeholder="Enter your first and last name" value={input.name} className='input' onChange={(e) =>handleChange(e)} required/>
         </div>
         <div>
-          <label> Email <span className='text-red-600'>*</span></label>
+          <label  className='dark:text-gray-300'> Email <span className='text-red-600'>*</span></label>
           <input name='email' type='email' placeholder="Enter your email" value={input.email} className='input'required onChange={(e) =>handleChange(e)}/>
         </div>
         <div>
-          <label>Phone Number</label>
+          <label  className='dark:text-gray-300'>Phone Number</label>
           <input name='phone'type='tel' placeholder="Enter your phone number" value={input.phone} className='input' onChange={(e) =>handleChange(e)}/>
         </div>
-        <label className='label'> I'm interested in <span className='text-red-600'>*</span></label>
+        <label className='label dark:text-gray-300'> I'm interested in <span className='text-red-600'>*</span></label>
           <textarea className='input h-[150px]' placeholder='Enter your message' name='textarea' value={input.textarea} onChange={(e) =>handleChange(e)} required></textarea>
-        <button type='submit' className='bg-[#f5b136] text-black mt-2 px-2.5 py-2 rounded-full hover:bg-[#f5a005]'>Submit</button>
+        <button type='submit' className='bg-[#f5b136] text-black mt-2 px-2.5 py-2 rounded-full hover:bg-[#f5a005] focus:outline-none focus:outline-yellow'>Submit</button>
       </form>
     </div>
   )
